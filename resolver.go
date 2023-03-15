9088832c39fe56917e6dfffdbdc6f25f7810a39b
Cristian Maglie <c.maglie@bug.st>
2023-03-15 14:27:40 +0100

    Generic dependency resolver. Added resolution strategies.

--- resolver.go
+++ resolver.go
@@ -15,22 +15,22 @@ type Dependency interface {
 }
 
 // Release represents a release, it must provide methods to return Name, Version and Dependencies
-type Release interface {
+type Release[D Dependency] interface {
 	GetName() string
 	GetVersion() *Version
-	GetDependencies() []Dependency
+	GetDependencies() []D
 }
 
-func match(r Release, dep Dependency) bool {
+// Releases is an array of Release
+type Releases[R Release[D], D Dependency] []R
+
+func match[R Release[D], D Dependency](r R, dep D) bool {
 	return r.GetName() == dep.GetName() && dep.GetConstraint().Match(r.GetVersion())
 }
 
-// Releases is a list of Release
-type Releases []Release
-
 // FilterBy return a subset of the Releases matching the provided Dependency
-func (set Releases) FilterBy(dep Dependency) Releases {
-	res := []Release{}
+func (set Releases[R, D]) FilterBy(dep D) Releases[R, D] {
+	res := []R{}
 	for _, r := range set {
 		if match(r, dep) {
 			res = append(res, r)
@@ -39,69 +39,136 @@ func (set Releases) FilterBy(dep Dependency) Releases {
 	return res
 }
 
-// SortDescent sort the Releases in this set in descending order (the lastest
+// SortDescending sort the Releases in this set in descending order (the lastest
 // release is the first)
-func (set Releases) SortDescent() {
+func (set Releases[R, D]) SortDescending() {
 	sort.Slice(set, func(i, j int) bool {
 		return set[i].GetVersion().GreaterThan(set[j].GetVersion())
 	})
 }
 
-// Archive contains all Releases set to consider for dependency resolution
-type Archive struct {
-	Releases map[string]Releases
+// SortAscending sort the Releases in this set in ascending order (the lastest
+// release is the latest)
+func (set Releases[R, D]) SortAscending() {
+	sort.Slice(set, func(i, j int) bool {
+		return set[i].GetVersion().LessThan(set[j].GetVersion())
+	})
 }
 
-// Resolve will try to depp-resolve dependencies from the Release passed as
-// arguent using a backtracking algorithm.
-func (ar *Archive) Resolve(release Release) []Release {
-	solution := map[string]Release{release.GetName(): release}
-	depsToProcess := release.GetDependencies()
-	return ar.resolve(solution, depsToProcess)
+// ResolutionStrategy is a resolution strategy for the dependency resolver
+type ResolutionStrategy int
+
+// MinimumVersionRequiredStrategy is a resolution strategy where the minimum
+// required version is taken from the available set. The computed solution
+// is stable, running the algorithm again will produce the same solution even
+// if new releases becomes available in the meantime.
+const MinimumVersionRequiredStrategy ResolutionStrategy = iota
+
+// MaximumVersionAvailableStrategy is a strategy where the latest available
+// version is taken from the available set. The computed solution will have
+// all the latest possible versions of the libraries. The solution is not
+// stable, it will probably require a lock file to be able to reproduce it
+// at a later time.
+const MaximumVersionAvailableStrategy ResolutionStrategy = iota
+
+// Resolve will try to resolve dependencies of the target Release using the
+// given resolution strategy.
+func (r *Releases[R, D]) Resolve(target R, strategy ResolutionStrategy) []R {
+	index := map[string]Releases[R, D]{}
+	for _, release := range *r {
+		index[release.GetName()] = append(index[release.GetName()], release)
+	}
+	ar := Resolver[R, D]{
+		index:         index,
+		solution:      map[string]R{target.GetName(): target},
+		depsToProcess: target.GetDependencies(),
+		strategy:      strategy,
+	}
+	return ar.resolve()
 }
 
-func (ar *Archive) resolve(solution map[string]Release, depsToProcess []Dependency) []Release {
-	debug("deps to process: %s", depsToProcess)
-	if len(depsToProcess) == 0 {
+// Resolver is a dependency resolver, it must be created with NewResolver method
+type Resolver[R Release[D], D Dependency] struct {
+	index         map[string]Releases[R, D]
+	solution      map[string]R
+	depsToProcess []D
+	strategy      ResolutionStrategy
+}
+
+// NewResolver creates a new Resolver
+func NewResolver[R Release[D], D Dependency]() *Resolver[R, D] {
+	return &Resolver[R, D]{
+		index: map[string]Releases[R, D]{},
+	}
+}
+
+func (ar *Resolver[R, D]) AddReleases(releases Releases[R, D]) {
+	for _, release := range releases {
+		ar.AddRelease(release)
+	}
+}
+
+func (ar *Resolver[R, D]) AddRelease(release R) {
+	ar.index[release.GetName()] = append(ar.index[release.GetName()], release)
+}
+
+func (ar *Resolver[R, D]) Resolve(target R, strategy ResolutionStrategy) []R {
+	ar.solution = map[string]R{target.GetName(): target}
+	ar.depsToProcess = target.GetDependencies()
+	ar.strategy = strategy
+	return ar.resolve()
+}
+
+func (ar *Resolver[R, D]) resolve() []R {
+	debug("deps to process: %s", ar.depsToProcess)
+	if len(ar.depsToProcess) == 0 {
 		debug("All dependencies have been resolved.")
-		res := []Release{}
-		for _, v := range solution {
+		res := []R{}
+		for _, v := range ar.solution {
 			res = append(res, v)
 		}
 		return res
 	}
 
 	// Pick the first dependency in the deps to process
-	dep := depsToProcess[0]
+	dep := ar.depsToProcess[0]
 	depName := dep.GetName()
 	debug("Considering next dep: %s", dep)
 
 	// If a release is already picked in the solution check if it match the dep
-	if existingRelease, has := solution[depName]; has {
+	if existingRelease, has := ar.solution[depName]; has {
 		if match(existingRelease, dep) {
 			debug("%s already in solution and matching", existingRelease)
-			return ar.resolve(solution, depsToProcess[1:])
+			ar.depsToProcess = ar.depsToProcess[1:]
+			return ar.resolve()
 		}
 		debug("%s already in solution do not match... rollingback", existingRelease)
 		return nil
 	}
 
 	// Otherwise start backtracking the dependency
-	releases := ar.Releases[dep.GetName()].FilterBy(dep)
+	releases := ar.index[dep.GetName()].FilterBy(dep)
 
 	// Consider the latest versions first
-	releases.SortDescent()
+	if ar.strategy == MaximumVersionAvailableStrategy {
+		releases.SortDescending()
+	} else {
+		releases.SortAscending()
+	}
 
 	debug("releases matching criteria: %s", releases)
 	for _, release := range releases {
 		debug("try with %s %s", release, release.GetDependencies())
-		solution[depName] = release
-		res := ar.resolve(solution, append(depsToProcess[1:], release.GetDependencies()...))
+		ar.solution[depName] = release
+		rolledBackDeps := ar.depsToProcess
+		ar.depsToProcess = append(ar.depsToProcess[1:], release.GetDependencies()...)
+		res := ar.resolve()
 		if res != nil {
 			return res
 		}
+		ar.depsToProcess = rolledBackDeps
 		debug("%s did not work...", release)
-		delete(solution, depName)
+		delete(ar.solution, depName)
 	}
 	return nil
 }
--- resolver_test.go
+++ resolver_test.go
@@ -18,10 +18,12 @@ type customDep struct {
 	cond Constraint
 }
 
+// GetName return the name of the dependency (implements the Dependency interface)
 func (c *customDep) GetName() string {
 	return c.name
 }
 
+// GetConstraint return the version contraints of the dependency (implements the Dependency interface)
 func (c *customDep) GetConstraint() Constraint {
 	return c.cond
 }
@@ -33,18 +35,21 @@ func (c *customDep) String() string {
 type customRel struct {
 	name string
 	vers *Version
-	deps []Dependency
+	deps []*customDep
 }
 
+// GetName return the name of the release (implements the Release interface)
 func (r *customRel) GetName() string {
 	return r.name
 }
 
+// GetVersion return the version of the release (implements the Release interface)
 func (r *customRel) GetVersion() *Version {
 	return r.vers
 }
 
-func (r *customRel) GetDependencies() []Dependency {
+// GetDependencies return the dependencies of the release (implements the Release interface)
+func (r *customRel) GetDependencies() []*customDep {
 	return r.deps
 }
 
@@ -52,88 +57,84 @@ func (r *customRel) String() string {
 	return r.name + "@" + r.vers.String()
 }
 
-func d(dep string) Dependency {
+func d(t *testing.T, dep string) *customDep {
 	name := dep[0:1]
 	cond, err := ParseConstraint(dep[1:])
-	if err != nil {
-		panic("invalid operator in dep: " + dep + " (" + err.Error() + ")")
-	}
+	require.NoError(t, err, "invalid operator in dep: %s (%s)", dep, err)
 	return &customDep{name: name, cond: cond}
 }
 
-func deps(deps ...string) []Dependency {
-	res := []Dependency{}
+func deps(t *testing.T, deps ...string) []*customDep {
+	res := []*customDep{}
 	for _, dep := range deps {
-		res = append(res, d(dep))
+		res = append(res, d(t, dep))
 	}
 	return res
 }
 
-func rel(name, ver string, deps []Dependency) Release {
+func rel(name, ver string, deps []*customDep) *customRel {
 	return &customRel{name: name, vers: v(ver), deps: deps}
 }
 
 func TestResolver(t *testing.T) {
-	b131 := rel("B", "1.3.1", deps("C<2.0.0"))
-	b130 := rel("B", "1.3.0", deps())
-	b121 := rel("B", "1.2.1", deps())
-	b120 := rel("B", "1.2.0", deps())
-	b111 := rel("B", "1.1.1", deps())
-	b110 := rel("B", "1.1.0", deps())
-	b100 := rel("B", "1.0.0", deps())
-	c200 := rel("C", "2.0.0", deps())
-	c120 := rel("C", "1.2.0", deps())
-	c111 := rel("C", "1.1.1", deps("B=1.1.1"))
-	c110 := rel("C", "1.1.0", deps())
-	c102 := rel("C", "1.0.2", deps())
-	c101 := rel("C", "1.0.1", deps())
-	c100 := rel("C", "1.0.0", deps())
-	c021 := rel("C", "0.2.1", deps())
-	c020 := rel("C", "0.2.0", deps())
-	c010 := rel("C", "0.1.0", deps("D"))
-	d100 := rel("D", "1.0.0", deps())
-	d120 := rel("D", "1.2.0", deps("E"))
-	e100 := rel("E", "1.0.0", deps())
-	arch := &Archive{
-		Releases: map[string]Releases{
-			"B": {b131, b130, b121, b120, b111, b110, b100},
-			"C": {c200, c120, c111, c110, c102, c101, c100, c021, c020, c010},
-			"D": {d100, d120},
-			"E": {e100},
-		},
-	}
+	b131 := rel("B", "1.3.1", deps(t, "C<2.0.0"))
+	b130 := rel("B", "1.3.0", deps(t))
+	b121 := rel("B", "1.2.1", deps(t))
+	b120 := rel("B", "1.2.0", deps(t))
+	b111 := rel("B", "1.1.1", deps(t))
+	b110 := rel("B", "1.1.0", deps(t))
+	b100 := rel("B", "1.0.0", deps(t))
+	c200 := rel("C", "2.0.0", deps(t))
+	c120 := rel("C", "1.2.0", deps(t))
+	c111 := rel("C", "1.1.1", deps(t, "B=1.1.1"))
+	c110 := rel("C", "1.1.0", deps(t))
+	c102 := rel("C", "1.0.2", deps(t))
+	c101 := rel("C", "1.0.1", deps(t))
+	c100 := rel("C", "1.0.0", deps(t))
+	c021 := rel("C", "0.2.1", deps(t))
+	c020 := rel("C", "0.2.0", deps(t))
+	c010 := rel("C", "0.1.0", deps(t, "D"))
+	d100 := rel("D", "1.0.0", deps(t))
+	d120 := rel("D", "1.2.0", deps(t, "E"))
+	e100 := rel("E", "1.0.0", deps(t))
 
-	a100 := rel("A", "1.0.0", deps("B>=1.2.0", "C>=2.0.0"))
-	a110 := rel("A", "1.1.0", deps("B=1.2.0", "C>=2.0.0"))
-	a111 := rel("A", "1.1.1", deps("B", "C=1.1.1"))
-	a120 := rel("A", "1.2.0", deps("B=1.2.0", "C>2.0.0"))
+	resolver := NewResolver[*customRel, *customDep]()
+	resolver.AddReleases(Releases[*customRel, *customDep]{b131, b130, b121, b120, b111, b110, b100})
+	resolver.AddReleases(Releases[*customRel, *customDep]{c200, c120, c111, c110, c102, c101, c100, c021, c020, c010})
+	resolver.AddReleases(Releases[*customRel, *customDep]{d100, d120})
+	resolver.AddReleases(Releases[*customRel, *customDep]{e100})
 
-	r1 := arch.Resolve(a100)
+	a100 := rel("A", "1.0.0", deps(t, "B>=1.2.0", "C>=2.0.0"))
+	a110 := rel("A", "1.1.0", deps(t, "B=1.2.0", "C>=2.0.0"))
+	a111 := rel("A", "1.1.1", deps(t, "B", "C=1.1.1"))
+	a120 := rel("A", "1.2.0", deps(t, "B=1.2.0", "C>2.0.0"))
+
+	r1 := resolver.Resolve(a100, MaximumVersionAvailableStrategy)
 	require.Len(t, r1, 3)
 	require.Contains(t, r1, a100)
 	require.Contains(t, r1, b130)
 	require.Contains(t, r1, c200)
 	fmt.Println(r1)
 
-	r2 := arch.Resolve(a110)
+	r2 := resolver.Resolve(a110, MaximumVersionAvailableStrategy)
 	require.Len(t, r2, 3)
 	require.Contains(t, r2, a110)
 	require.Contains(t, r2, b120)
 	require.Contains(t, r2, c200)
 	fmt.Println(r2)
 
-	r3 := arch.Resolve(a111)
+	r3 := resolver.Resolve(a111, MaximumVersionAvailableStrategy)
 	require.Len(t, r3, 3)
 	require.Contains(t, r3, a111)
 	require.Contains(t, r3, b111)
 	require.Contains(t, r3, c111)
 	fmt.Println(r3)
 
-	r4 := arch.Resolve(a120)
+	r4 := resolver.Resolve(a120, MaximumVersionAvailableStrategy)
 	require.Nil(t, r4)
 	fmt.Println(r4)
 
-	r5 := arch.Resolve(c010)
+	r5 := resolver.Resolve(c010, MaximumVersionAvailableStrategy)
 	require.Contains(t, r5, c010)
 	require.Contains(t, r5, d120)
 	require.Contains(t, r5, e100)
